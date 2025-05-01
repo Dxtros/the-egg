50224cfe86c4e6c0cd00b6382f3489b315f14160
Delaski <delaskidexter@gmail.com>
2025-05-01 13:17:11 -0700

    Got jquery to work
    First two sentences in story

--- main.js
+++ main.js
@@ -1,73 +1,77 @@
-
-
 $(document).ready(function () {
-
-    //story sections
-
+    // Story sections array
     const storyContent = [
         {
-        id: 1,
-        narrator: 'You were on your way home when you died',
-        narratorClass: '.intro',
-        person: '',
-        personClass: '',
-        story:'',
-        storyClass: '',
-        hasButton: false,
-        timer: '3000',
-        linger: false
+            id: 1,
+            narrator: 'You were on your way home when you died.',
+            narratorClass: 'intro',
+            person: '',
+            personClass: '',
+            story: '',
+            storyClass: '',
+            hasButton: false,
+            timer: 3000,
+            linger: false
         },
         {
             id: 2,
-            narrator: 'It was a car accident, nothing particularly remarkable',
-            narratorClass: '.intro',
+            narrator: 'It was a car accident. Nothing particularly remarkable, but fatal nonetheless.',
+            narratorClass: 'intro-narrator',
             person: '',
             personClass: '',
-            story:'',
+            story: '',
             storyClass: '',
             hasButton: false,
-            timer: '3000',
+            timer: 3000,
             linger: false
-            },
-
+        },
+      
     ];
 
     let currentIndex = 0;
+    let storyTimer;
 
-    function displayStorySegment(index){
-        const segment = storyContent[index];
-
-        $('.begin-button').on("click", function(){
-
-function advanceStory()
-        }
-
-    )
+    // Handle the begin button on the landing page
+    $('.button-begin').on('click', function() {
+        // Replace content on the same page
+        $('main').html(`
+            <div class="intro-div">
+                <h1 class="center-text narrator intro"></h1>
+            </div>
+        `);
         
-if (segment.index > 3) {
-        $('.center-text').html('<p class="${segment.narrator}</p>');
+        // Start the story
+        displayIntroStoryContent(currentIndex);
+    });
+
+    // Function to display intro story content
+    function displayIntroStoryContent(index) {
+        // Clear any existing timers
+        if (storyTimer) {
+            clearTimeout(storyTimer);
+        }
+        
+        // Check if we're at the end of the content array
+        if (index >= storyContent.length) {
+            setupDialogueSection();
+            return;
+        }
+        
+        // Get the current segment
+        const segment = storyContent[index];
+        
+        // Display the narrator text 
+        $('.narrator.intro').text(segment.narrator);
+        
+      
+            
+if (segment.timer > 0) {
+            // If no button and has timer, automatically advance after delay
+            storyTimer = setTimeout(function() {
+                currentIndex++;
+                displayIntroStoryContent(currentIndex);
+            }, segment.timer);
+        
     }
 
-
-    function advanceStory(){
-        storyContent.index ++;
-        alert("story increased");
-    }
-}
-
-
-
-//     if ($('.story-content').length === 0) {
-//         $('main').html(`
-//             <div class="story-content">
-//                 <div class="left"></div>
-//                 <div class="right"></div>
-//                 <div class="button-area"></div>
-//             </div>
-//         `);
-//     }
-
-// });
-
-
-
+}})
