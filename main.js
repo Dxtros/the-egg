e8825e4b14cd71bf76c8bcfb674441be404baedd
Dexter Delaski <git@dextros.net>
2025-05-06 15:44:33 -0700

    working on section1 displaying and managment

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
     // Story sections array
     let currentIndex = 0;
+    let contentS1Index = 0;
     let storyTimer;
     let timerEnd = false;
+    //skip with keys
+
+    document.addEventListener('keydown', function (event) {
+        console.log('Key pressed:', event.key);
+
+
+        // You can also check for arrow keys, space, etc.
+        if (event.key === 'ArrowRight') {
+            console.log('Right arrow was pressed');
+            currentIndex++;
+        }
+        if (event.key === 'ArrowLeft') {
+            console.log('Right arrow was pressed');
+            currentIndex--;
+        }
+    });
+
 
     const introContent = [
         {
@@ -36,18 +54,33 @@ $(document).ready(function () {
     const contentS1 = [
         {
             id: 1,
-            narrator: '',
-            narratorClass: '',
+            narrator: 'lorem lorem',
+            narratorClass: 'intro-narrator',
             anim: 'animate__animated animate__fadeIn',
             outAnim: 'animate__animated animate__fadeOut',
-            person: '“What… what happened?”<span class="small-narrator">You asked.</span> “Where am I?”',
-            personClass: '',
+            person: '“What… what happened?” <span class="small-narrator">You asked.</span> “Where am I?”',
+            personClass: 'person',//write css class
             story: '',
             storyClass: '',
-            hasButton: true, // write function for this
-            timer: '',
+            hasButton: true, 
+            timer: '3000',
             lingerN: false,
             lingerP: false //write function for this
+        },
+        {
+            id: 2,
+            narrator: 'lorem lorem',
+            narratorClass: 'intro-narrator',
+            anim: 'animate__animated animate__fadeIn',
+            outAnim: 'animate__animated animate__fadeOut',
+            person: '“What… what happened?” <span class="small-narrator">You asked.</span> “Where am I?”',
+            personClass: 'person',//write css class
+            story: '',
+            storyClass: '',
+            hasButton: true, 
+            timer: '3000',
+            lingerN: false,
+            lingerP: true //write function for this
         }
     ];
 
@@ -73,16 +106,16 @@ $(document).ready(function () {
     function displayIntroStoryContent(index) {
         console.log("displayIntroStoryContent called with index:", index);
         // Clear any existing timers and replace with timer value
-        if (storyTimer) {
 
-            clearTimeout(storyTimer);
 
-        }
+        clearTimeout(storyTimer);
 
-        // Check if we're at the end of the content array
+
+
+        // Check if at the end of the content array
         if (index >= introContent.length) {
             console.log("End of content reached, setting up dialogue section");
-            update_DialogueSection();
+            update_DialogueSection(contentS1Index);
             return;
         }
 
@@ -95,16 +128,10 @@ $(document).ready(function () {
         // Display the narrator text 
 
         $('main').html(
-
-
             `
- 
-            
-
             <div class="intro-div">
                 <h1 class="${segment.narratorClass} ${segment.anim}">${segment.narrator}</h1>
             </div>
-
         `);
 
 
@@ -152,38 +179,53 @@ $(document).ready(function () {
 
     }
 
-    function update_DialogueSection() {
-        const segment = contentS1[index]
+
+    //===Dialogue section 1 begins===//
+
+
+    function update_DialogueSection(contentS1Index) {
+        console.log('Update dialogue section triggerd');
+        const segment1 = contentS1[contentS1Index] //timer logic is a mess
+        console.log(segment1);
         //checks for story end
-        if (index >= contentS1.length) {
+        if (contentS1Index >= contentS1.length) { 
             console.log("End of content reached, setting up END");
             endStory();//does not exist yet
             return;
         }
 
 
-        // function to set linger
-        if (segment.lingerN) {
-            const narrator = document.querySelector("${segment.narratorClass}");
+        // checking to set linger
+        if (segment1.lingerN) {
+            const narrator = document.querySelector("${segment1.narratorClass}");
             narrator.classList.add('fade-text')
 
         }
 
-        if (segment.lingerP) {
-            const person = document.querySelector("${segment.personClass}");
-            person.classList.add('fade-text')
+        if (segment1.lingerP) {
+            const currentClass ="${segment1.personClass}";
+            const person = document.querySelector("currentClass");
+            person.classList.add("fade-text") //fix this error on finding class
         }
-        // display button or write timer logic here-- if button display it here and on click trigger button section
-        if (segment.button) {
+        // display button or timer logic here-- if button display it here and on click trigger button section
+        if (segment1.hasButton) {
             //display button and trigger on click
-
+            console.log(segment1.hasButton);
             //need to make .button-cont class
-            $('.main').append(`
-                    <div class=".button-cont"></div>
-                `);
+           
+            $('main').html(
+                `
+                <div class="segment1-wrapper">
+                    <p class="${segment1.personClass} ${segment1.anim}">${segment1.person}</p>
+                </div>
+            `);
+            $('main').append(`
+
+                <img class="button-cont " src="/img/cont-button-v1.svg">
+              `);
             $('.button-cont').on('click', function () {
-                console.log("cont clicked, currentIndex:", currentIndex);
-                buttonDialogue(segment);
+                console.log("cont clicked, currentIndex:", segment1);
+                buttonDialogue(segment1);
             }
                 //writeDialogueSection(segment);
 
@@ -191,17 +233,57 @@ $(document).ready(function () {
         }
         else {
             console.log('timer dialgue was triggered');
-            //timer logic goes here same as intro
+            //timer logic goes here same as intro?
+            //needs to be fixed right now it just repeats and doesnt reach .length
+            if (storyTimer>=segment1.timer) {
+
+                clearTimeout(storyTimer);
+
+            }
+
+            console.log("Displaying segment:", segment1);
+            // Display the narrator text 
+
+            $('main').html(
+                `
+                <div class="segment1-wrapper">
+                    <p class="${segment1.narratorClass} ${segment1.anim}">${segment1.narrator}</p>
+                </div>
+            `);
+
+            console.log("Setting timeout for next segment with index:", contentS1Index + 1);
+            storyTimer = setTimeout(function () {
+                const element = document.querySelector('p');
+                console.log('timer end')
+                timerEnd = true;
+                fadeOut(element, timerEnd);
+
+
+                element.addEventListener('animationend', () => {
+                    // Do something when the animation finishes
+                    console.log('fade out ended', element.classList);
+
+                    update_DialogueSection(contentS1Index+1);
+
+                }
+                );
+
+
+            }, segment1.timer);
         }
 
-
-
-        function buttonDialogue(s) {
-
-            //display next text from button click (narrator usually)
-
-        }
     }
 
 
-})
+
+    function buttonDialogue(s) {
+        console.log('button dialogue triggered');
+
+        update_DialogueSection(contentS1Index+1);
+        //display next text from button click (narrator usually)
+
+    }
+}
+
+
+);
